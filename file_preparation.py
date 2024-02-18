09bd5a85f3ff8e4eff9915a951bfa4c86a0aa106
alex <you@example.com>
2024-02-18 16:18:14 +0300

    intermed commit

---
+++ file_preparation.py
@@ -0,0 +1,31 @@
+import argparse
+import pickle
+
+from langchain.vectorstores.faiss import FAISS
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings.sentence_transformer import SentenceTransformerEmbeddings
+from telegram_json_loader import TgJsonLoader
+
+embedding_function = SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2")
+
+def prepare_data_file(data_file_path):
+    loader = TgJsonLoader(data_file_path)
+    loaded_data = loader.load()
+
+    splitter = CharacterTextSplitter(separator="\n\n", chunk_size=512, chunk_overlap=20)
+    document = splitter.split_documents(loaded_data)
+
+    faiss_vectorstore = FAISS.from_documents(document, embedding_function)
+
+    # create preprocessed file
+    with open("preprocessed_data.pkl", "wb") as f:
+        pickle.dump(faiss_vectorstore, f)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('json_file_path', type=str,
+                        help='Json(*.json) file from telegram or other sources')
+    args = parser.parse_args()
+    file_path = args.file_path
+    prepare_data_file(file_path)
--- run_app.py
+++ run_app.py
@@ -6,12 +6,12 @@ if __name__ == "__main__":
     with open("preprocessed_data.pkl", "rb") as f:
         vectordata = pickle.load(f)
     new_chain = create_chain(vectordata)
-    chat_history = []
-    print("Chat with your docs!")
+    history = []
+    
     while True:
         print("Ваш вопрос:")
         question = input()
-        result = new_chain({"question": question, "chat_history": chat_history})
-        chat_history.append((question, result["answer"]))
+        result = new_chain({"question": question, "chat_history": history})
+        history.append((question, result["answer"]))
         print("AI ассистент:")
         print(result["answer"])
